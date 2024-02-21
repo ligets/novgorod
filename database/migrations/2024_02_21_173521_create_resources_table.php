c4e7584aedaad5bc66d12c0c7ffb7e13db393bfa
Kostya <kostyabaigulov@gmail.com>
2024-02-21 21:23:32 +0300

    Создание миграций(БД)

---
+++ database/migrations/2024_02_21_173521_create_resources_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('format', 15);
+            $table->json('tegs')->nullable();
+            $table->string('metadata', 255);
+            $table->boolean('in_album')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        DB::statement('ALTER TABLE resources ADD data LONGBLOB AFTER in_album');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
